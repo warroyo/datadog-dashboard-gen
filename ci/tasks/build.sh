fe5af5d11158660e1d80d1c7c22a7b25e4ed4d4e
virtmerlin <mglynn@pivotal.io>
2016-03-14 08:51:00 -0500

    Add Windows & Darwin Cross Compliles

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -19,8 +19,10 @@ pushd $fullpath > /dev/null
   git_rev=`git rev-parse --short HEAD`
   version="${semver}-${git_rev}-${timestamp}"
 
-  echo -e "\n building artifact..."
-  go build -ldflags "-X main.version=${version}" -o "out/datadog-dashboard-gen-linux-amd64"
+  echo -e "\n building artifacts..."
+  GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=${version}" -o "out/datadog-dashboard-gen-linux-amd64"
+  GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=${version}" -o "out/datadog-dashboard-gen-darwin-amd64"
+  GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=${version}" -o "out/datadog-dashboard-gen-win-amd64"
 
   echo -e "\n sha1 of artifact..."
   sha1sum out/datadog-dashboard-gen-linux-amd64
